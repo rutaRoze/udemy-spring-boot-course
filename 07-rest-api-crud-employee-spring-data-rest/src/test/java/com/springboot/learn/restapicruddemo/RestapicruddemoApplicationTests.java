6ffbb465fa1f0aff3953a0d781403ad72911377c
Rūta Rožėnė <rozene.ruta@gmail.com>
2024-01-14 23:28:17 +0200

    controller and service deleted because of added data rest dependency

---
+++ 07-rest-api-crud-employee-spring-data-rest/src/test/java/com/springboot/learn/restapicruddemo/RestapicruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.learn.restapicruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapicruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
